8cac41885ad2c74d36f3d85429e9eec3b8a71821
Poly <marynczak.bartlomiej@gmail.com>
2021-09-11 17:34:41 +0200

    Fix spack entry point

--- spack.config.js
+++ spack.config.js
@@ -2,7 +2,7 @@ const { config } = require("@swc/core/spack");
 
 module.exports = config({
   entry: {
-    web: __dirname + "/src/index.tsx",
+    web: __dirname + "/src/index.ts",
   },
   output: {
     path: __dirname + "/dist",
