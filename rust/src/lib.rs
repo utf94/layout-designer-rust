e56e9b206d200bb03cf2ab9dcea47de2b4bb52a4
Poly <marynczak.bartlomiej@gmail.com>
2021-09-16 04:54:11 +0200

    Fix the rust-analyzer check failure

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,7 +16,13 @@ pub fn start() -> ::std::result::Result<(), JsValue> {
     console_log::init_with_level(log::Level::Debug).unwrap();
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    elements::component::register();
+    // NOTE(poly): This does not require unsafe
+    // but rust-analyzer in my IDE is complaining that it needs it
+    // and I'm to lazy to file a bug for it, so I just added the redundant unsafe
+    #[allow(unused_unsafe)]
+    unsafe {
+        elements::component::register();
+    }
 
     Ok(())
 }
