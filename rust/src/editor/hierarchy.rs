4c8e4c2fc661d4e3041445a997ad6cb61eb15b49
Hafiz Ammar Siddiqui <ammar26@gmail.com>
2021-09-21 22:13:04 +0500

    Hierarchy (work in progress)

--- rust/src/editor/hierarchy.rs
+++ rust/src/editor/hierarchy.rs
@@ -1,37 +1,35 @@
 #![allow(unused)]
-
+/// Include relevent crates and modules
 use gloo_events::EventListener;
 use wasm_bindgen::JsCast;
 use web_sys::{Element, HtmlElement};
-
 use crate::page::Page;
 
+/// Hierarchy Struct to represent inspector in editor
 pub struct Hierarchy {
-    /// Root html element of the Inspector
+    /// Root html element of the Hierarchy
     html_element: HtmlElement,
 }
 
+/// Methods for Hierarchy Struct
 impl Hierarchy {
-    /// Initialize the workspace
+    /// Create new instance of the hierarchy
     pub fn new() -> Self {
         let document = web_sys::window().unwrap().document().unwrap();
-
         let html_element = document.get_element_by_id("inspector-tree").unwrap();
         let html_element: HtmlElement = html_element.dyn_into().unwrap();
-
-        Self { html_element }
+        Self { 
+            html_element,
+        }
     }
 
     /// Determines whether the tree contains a given html element
     pub fn contains(&self, elm: &Element) -> bool {
         self.html_element.contains(Some(elm))
     }
-
-    pub fn insert_page(&mut self, page: &Page) {
-        //
-    }
 }
 
+/// Partial equivalence relation for Hierarchy Struct
 impl PartialEq<Element> for Hierarchy {
     fn eq(&self, html_element: &Element) -> bool {
         let root: &Element = self.html_element.as_ref();
